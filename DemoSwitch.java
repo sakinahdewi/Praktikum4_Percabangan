f3bf436732d9ef91a6ba00b6d0d10cf7193dee2e
sakinahdewi <32097423+sakinahdewi@users.noreply.github.com>
2017-09-29 00:06:54 -0600

    Add files via upload

---
+++ DemoSwitch.java
@@ -0,0 +1,19 @@
+public class DemoSwitch {
+
+    public static void main(String[] args) {
+        int grade = 80;
+        switch (grade) {
+            case 100:
+             System.out.println("Excellent!");
+                 break;
+            case 90:
+            System.out.println("Good Job!");
+                 break;
+            case 80:
+            System.out.println("Study Harder!");
+                 break;
+            default:
+            System.out.println("sorry, you failed.");
+        }
+    }
+}
